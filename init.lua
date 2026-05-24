361d814a7cc167d3528b98ea015812e2756e692c
Kostya <happy.boat9793@fastmail.com>
2026-05-24 16:38:45 +0100

    delete accidentally copied lua files, up to date files are in the `nvim` subdir

--- init.lua
+++
@@ -1,6 +0,0 @@
-require('config.global')
-require('config.options')
-require('config.lazy')
-require('config.keymap')
-require('config.autocmds')
-require('config.lsp')
--- lua/plugins/tagbar.lua
+++
@@ -1,7 +0,0 @@
-return {
-    'preservim/tagbar',
-    keys = {
-        { '<F9>', vim.cmd.TagbarToggle }
-    }
-
-}
